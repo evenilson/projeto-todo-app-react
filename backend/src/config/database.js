1d6873a906c902c29b67dbaf0f78bb0d95412eee
evenilson <evenilsonlp@gmail.com>
2020-11-05 19:15:26 -0300

    corrigindo erro de conexão com o banco

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose')
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect('mongodb://localhost/todo');
