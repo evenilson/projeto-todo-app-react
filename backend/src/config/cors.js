8833f45441415e3afc8b671eca58ed9fec3f924e
evenilson <evenilsonlp@gmail.com>
2020-11-05 20:19:31 -0300

    habilitando o CORS + backend finalizado

---
+++ backend/src/config/cors.js
@@ -0,0 +1,8 @@
+const { get } = require("mongoose")
+
+module.exports = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type')
+    next()
+}
