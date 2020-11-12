0728026f25f7459344e778d113f9f5913d186ad8
evenilson <evenilsonlp@gmail.com>
2020-11-11 22:02:20 -0300

    configurando as rotas

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,15 +2,14 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from '../todo/todo'
-import About from '../about/about'
+
 import Menu from '../template/menu'
+import Routes from './routes'
 
 export default props => (
     <div className='container'>
         <Menu/>
-        <Todo />
-        <About />
+        <Routes />
     </div>
 )
 
