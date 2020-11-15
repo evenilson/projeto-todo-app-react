ce3ef7083c59535182cd7d0b44224904807b09ce
evenilson <evenilsonlp@gmail.com>
2020-11-15 20:54:28 -0300

    criando estrutura do formulario

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export default props => (
-    <div>
-        <h1>Formulario</h1>        
+    <div role='form' className='todoForm'>
+        <div className='col-xs-12 col-sm-9 col-md-10'>
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>                
+        </div>
+
+        <div className='col-xs-12 col-sm-3 col-md-2'>
+            <button className='btn btn-success'>
+                <i className='fa fa-plus'></i>
+            </button>
+        </div>                
     </div>
 )
