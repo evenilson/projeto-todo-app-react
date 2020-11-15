181e956a9e90f4289f57b27be59724a73839cabc
evenilson <evenilsonlp@gmail.com>
2020-11-15 20:42:40 -0300

    criando todoList e todoForm

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>Formulario</h1>        
+    </div>
+)
---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props =>(
+    <div>
+        <h1>Lista</h1>
+    </div>
+)
