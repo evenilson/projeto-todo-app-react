375609c2a42c1f1c12ff36c7f190c229e36e8bc3
evenilson <evenilsonlp@gmail.com>
2020-11-16 20:29:28 -0300

    fazensdo o evento onChange

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -8,18 +8,30 @@ export default class Todo extends Component {
 
     constructor(props){
         super(props)
+
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
 
     handleAdd(){
-        console.log(this)
+        console.log(this.state.description)
+    }
+
+    handleChange(e){
+        this.setState({
+            ...this.state, description: e.target.value
+        })
     }
 
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoForm description={this.state.description} 
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}/>
                 <TodoList />
             </div>
         )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,7 +6,9 @@ import IconButton from '../template/iconButton'
 export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>                
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa'
+                onChange={props.handleChange}
+                value={props.description}></input>                
         </Grid>
 
         <Grid cols='12 3 2'>
