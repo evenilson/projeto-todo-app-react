22f8ea98b3d07cc8a6cf1e5fc9f7fc40f437785a
evenilson <evenilsonlp@gmail.com>
2020-11-16 21:36:06 -0300

    fazendo a consulta e exclusaão dos TODOs

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -16,18 +16,33 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({
+                ...this.state, description:'', list: resp.data
+            }))
     }
 
     handleAdd(){
         const description = this.state.description
         axios.post(URL, {description})
-        .then(resp => console.log('Funcionou!'))
+        .then(resp => this.refresh())
     }
 
     handleChange(e){
         this.setState({
             ...this.state, description: e.target.value
         })
+    }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
     render(){
@@ -37,7 +52,7 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description} 
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd}/>
-                <TodoList />
+                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
             </div>
         )
     }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
-export default props =>(
-    <div>
-        <h1>Lista</h1>
-    </div>
-)
+import IconButton from '../template/iconButton'
+
+export default props =>{
+
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style='danger' icon='trash-o' 
+                        onClick={() => props.handleRemove(todo)}></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+    return(
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>    
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>       
+        </table>
+    )
+}
