9ca49a887d34872494330a2454c775bfe54a2623
evenilson <evenilsonlp@gmail.com>
2020-11-18 18:50:39 -0300

    adicionando teclas de atalho

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -3,21 +3,33 @@ import React from 'react'
 import Grid from '../template/crid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role='form' className='todoForm'>
-        <Grid cols='12 9 9'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'
-                onChange={props.handleChange}
-                value={props.description}></input>                
-        </Grid>
+export default props => {
 
-        <Grid cols='12 3 3'>
-            <IconButton style='primary' icon='plus' 
-                onClick={props.handleAdd}></IconButton>
-            <IconButton style='info' icon='search' 
-                onClick={props.handleSeach}></IconButton>
-            <IconButton style='default' icon='close' 
-                onClick={props.handleClear}></IconButton>
-        </Grid>
-    </div>
-)
+    const keyHandler = (e) => {
+        if(e.key === 'Enter'){
+            e.shiftKey ? props.handleSeach() : props.handleAdd()
+        }else if(e.key === 'Escape'){
+            props.handleClear()
+        }
+    }
+
+    return (
+        <div role='form' className='todoForm'>
+            <Grid cols='12 9 9'>
+                <input id='description' className='form-control' placeholder='Adicione uma tarefa'
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                    value={props.description}></input>
+            </Grid>
+
+            <Grid cols='12 3 3'>
+                <IconButton style='primary' icon='plus'
+                    onClick={props.handleAdd}></IconButton>
+                <IconButton style='info' icon='search'
+                    onClick={props.handleSeach}></IconButton>
+                <IconButton style='default' icon='close'
+                    onClick={props.handleClear}></IconButton>
+            </Grid>
+        </div>
+    )
+}
